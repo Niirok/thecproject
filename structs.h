16ab1267a6e4743b88c5a94b467ea06ab22b3295
Niirok <anthony_youssef@hotmail.fr>
2015-05-01 23:14:29 +0200

    suppression en vu du 1er merge

--- structs.h
+++ structs.h
@@ -4,12 +4,11 @@
 #include "defs.h"
  
 /* Structures qui seront utilisées pour gérer le jeu */
- 
-// Structure pour gérer l'input (clavier puis joystick) 
-typedef struct Input
+
+typedef struct Input // Structure pour gérer l'input clavier
 {
  
-    int left, right, up, down, jump, attack, enter, erase, pause;
+    int shoot, color1,color2,color3,color4,color5,color6,color7,color8,color9,pause,mute,changeBack;
  
 } Input;
  
